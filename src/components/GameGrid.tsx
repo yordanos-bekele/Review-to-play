a18725cf2313de22f6417bb3a35c8455a7f849df
Yordanos <yordanosbeke@gmail.com>
2024-10-06 10:21:30 +0300

    cleaning up genrelist and gamegrid components

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,23 @@
-import { SimpleGrid, Text } from '@chakra-ui/react'
-import GameCard from './GameCard';
-import GameCardContainer from './GameCardContainer';
-import useGame from '../hooks/UseGame';
-import { GameQuery } from '../App';
-interface Props{
-    gameQuery : GameQuery;
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import useGame from "../hooks/UseGame";
+import { GameQuery } from "../App";
+interface Props {
+  gameQuery: GameQuery;
 }
-function GameGrid({gameQuery}:Props) {
-    const {data,error} = useGame(gameQuery);
+function GameGrid({ gameQuery }: Props) {
+  const { data, error } = useGame(gameQuery);
+  if (error) return <Text>{error}</Text>;
   return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm:1, md:2 , lg:3 , xl:4}} spacing={3}>
-                {data.map((game)=>(
-                    <GameCardContainer>
-                        <GameCard  key={game.id} game={game}/>
-                    </GameCardContainer>
-                ))}
-            </SimpleGrid>
-        </>
-
-  )
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3}>
+      {data.map((game) => (
+        <GameCardContainer>
+          <GameCard key={game.id} game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
+  );
 }
 
-export default GameGrid
+export default GameGrid;
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,31 +1,42 @@
-import { Button, HStack, Image, List, ListItem} from '@chakra-ui/react';
-import useGenre from '../hooks/UseGenre';
-import Loader from './Loader';
-import genre from '../interfaces/Genre';
-import getCroppedImageUrl from '../services/image-url';
-interface Props{
-  handleGenre: (Genre:genre)=> void
-  selectedGenre: genre | null
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import useGenre from "../hooks/UseGenre";
+import Loader from "./Loader";
+import genre from "../interfaces/Genre";
+import getCroppedImageUrl from "../services/image-url";
+interface Props {
+  handleGenre: (Genre: genre) => void;
+  selectedGenre: genre | null;
 }
-const GenreList = ({handleGenre, selectedGenre}: Props) => {
-const {data,isLoading} = useGenre();
-  if (isLoading) return <Loader/>
+const GenreList = ({ handleGenre, selectedGenre }: Props) => {
+  const { data, isLoading } = useGenre();
+  if (isLoading) return <Loader />;
   return (
-    
     <List>
-        {data.map(genre=>(
-            <ListItem key={genre.id} paddingY={2}>
-              <HStack>
-                <Image src={getCroppedImageUrl(genre.image_background)} boxSize='32px' borderRadius={3}/>
-                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} fontSize={'lg'} variant={'link'} className='hover:translate-x-2' onClick={()=>handleGenre(genre)}>{genre.name}</Button>
-              </HStack>
-            </ListItem>
-        ))}
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={2}>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={3}
+              textAlign={"left"}
+            />
+            <Button
+              whiteSpace={"normal"}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              fontSize={"lg"}
+              variant={"link"}
+              className="hover:translate-x-2"
+              onClick={() => handleGenre(genre)}
+              textAlign={"left"}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
-    
-  )
-}
+  );
+};
 
-export default GenreList
-
-
+export default GenreList;
